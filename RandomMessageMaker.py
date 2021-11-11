576c9f5a42d39a59a11019a9dad6502d30392966
NoodleSushi <ytmikesstuffs@gmail.com>
2021-11-11 17:43:15 +0800

    RMM Init

--- RandomMessageMaker.py
+++ RandomMessageMaker.py
@@ -1,23 +1,17 @@
-from typing import List, Dictionary
+from typing import List, Dict, Union
 from enum import Enum
+import random
 
 class RandomMessageMaker():
     def __init__(self) -> None:
+        self.brain = {'START': [], 'TOKENS': []}
 
-        pass
-
-    """
-    "START" = [],
-    ... = [],
-    ... = [],
-    
-    """
-
-    def brain_generator(self, message_data: List[str]) -> Dictionary:
-        brain: Dictionary = {
-            "START": []
+    @staticmethod
+    def brain_generator(message_data: List[str]) -> Dict:
+        brain: Dict = {
+            'START': []
         }
-        token2id: Dictionary = {}
+        token2id: Dict = {}
         id2token: List = []
 
         for message in message_data:
@@ -28,12 +22,12 @@ class RandomMessageMaker():
                 token: str = message_tokens[token_idx]
                 # add token if not found
                 if not token in token2id:
-                    token2id[token] = token2id[len(id2token)]
+                    token2id[token] = len(id2token)
                     id2token.append(token)
                 id: int = token2id[token]
                 
                 if token_idx == 0:
-                    brain["START"].append(id)
+                    brain['START'].append(id)
                 else:
                     if not past_id in brain:
                         brain[past_id] = []
@@ -42,6 +36,19 @@ class RandomMessageMaker():
                 past_id = id
             if not past_id in brain:
                 brain[past_id] = []
-            brain[past_id].append(id)
-
+            brain[past_id].append(-1)
+        brain['TOKENS'] = id2token
         return brain
+
+    def set_brain(self, new_brain: Dict) -> None:
+        self.brain = new_brain
+    
+    def generate_message(self, brain: Union[Dict, None] = None) -> str:
+        if brain == None:
+            brain = self.brain
+        message: str = ""
+        id = random.choice(brain['START'])
+        while (id != -1):
+            message += brain['TOKENS'][id] + " "
+            id = random.choice(brain[id])
+        return message[:-1]
