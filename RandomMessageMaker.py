6493389b4d963124702a910e87fe2fad4ba24ec4
NoodleSushi <ytmikesstuffs@gmail.com>
2021-11-11 21:43:29 +0800

    Refactored RMM

--- RandomMessageMaker.py
+++ RandomMessageMaker.py
@@ -2,53 +2,84 @@ from typing import List, Dict, Union
 from enum import Enum
 import random
 
+class MarkovCStateType(Enum):
+    Start = 0
+    Token = 1
+    End = 2
+
+class MarkovC():
+    def __init__(self) -> None:
+        self.data = {-1: []}
+        self.node_id_lookup: Dict = {}
+        self.token_list: List = []
+        self.is_appending_token: bool = False
+        self.last_appended_id: int = 0
+        self.node_id: int = -1
+        self.state_type: MarkovCStateType = MarkovCStateType.Start
+    
+    def append_token(self, token: str) -> None:
+        if not token in self.node_id_lookup:
+            self.node_id_lookup[token] = len(token)
+            self.token_list.append(token)
+        id: int = self.node_id_lookup[token]
+        if not self.is_appending_token:
+            self.data[-1].append(id)
+            self.is_appending_token = True
+        else:
+            self.__allocate_last_id()
+            self.data[self.last_appended_id].append(id)
+        self.last_appended_id = id
+    
+    def end_append(self) -> None:
+        self.is_appending_token = False
+        self.__allocate_last_id()
+        self.data[self.last_appended_id].append(-1)
+    
+    def __allocate_last_id(self) -> None:
+        if not self.last_appended_id in self.data:
+            self.data[self.last_appended_id] = []
+    
+    def transition_random_node(self) -> None:
+        self.node_id = random.choice(self.data[self.node_id])
+        self.state_type = MarkovCStateType.End if self.node_id == -1 else MarkovCStateType.Start
+    
+    def get_node_token(self) -> str:
+        return self.token_list[self.node_id]
+
+    def get_state_type(self) -> MarkovCStateType:
+        return self.state_type
+    
+    def reset_state(self) -> None:
+        self.node_id = -1
+        self.state_type = MarkovCStateType.Start
+
+
 class RandomMessageMaker():
     def __init__(self) -> None:
-        self.brain = {'START': [], 'TOKENS': []}
+        self.chain: MarkovC = MarkovC()
 
     @staticmethod
-    def brain_generator(message_data: List[str]) -> Dict:
-        brain: Dict = {
-            'START': []
-        }
-        token2id: Dict = {}
-        id2token: List = []
+    def generate_chain(message_data: List[str]) -> Dict:
+        chain: MarkovC = MarkovC()
 
         for message in message_data:
             message_tokens: List[str] = message.split()
-            past_token: str
-            past_id: int
-            for token_idx in range(len(message_tokens)):
-                token: str = message_tokens[token_idx]
-                # add token if not found
-                if not token in token2id:
-                    token2id[token] = len(id2token)
-                    id2token.append(token)
-                id: int = token2id[token]
-                
-                if token_idx == 0:
-                    brain['START'].append(id)
-                else:
-                    if not past_id in brain:
-                        brain[past_id] = []
-                    brain[past_id].append(id)
-                past_token = token
-                past_id = id
-            if not past_id in brain:
-                brain[past_id] = []
-            brain[past_id].append(-1)
-        brain['TOKENS'] = id2token
-        return brain
+            for token in message_tokens:
+                chain.append_token(token)
+            chain.end_append()
+        return chain
 
-    def set_brain(self, new_brain: Dict) -> None:
-        self.brain = new_brain
+    def set_chain(self, new_chain: Dict) -> None:
+        self.chain = new_chain
     
-    def generate_message(self, brain: Union[Dict, None] = None) -> str:
-        if brain == None:
-            brain = self.brain
-        message: str = ""
-        id = random.choice(brain['START'])
-        while (id != -1):
-            message += brain['TOKENS'][id] + " "
-            id = random.choice(brain[id])
-        return message[:-1]
+    def generate_message(self, chain: Union[MarkovC, None] = None) -> str:
+        if chain == None:
+            chain = self.chain
+        chain.reset_state()
+        chain.transition_random_node()
+        message: str = chain.get_node_token()
+        chain.transition_random_node()
+        while (chain.get_state_type() != MarkovCStateType.End):
+            message += " " + chain.get_node_token()
+            chain.transition_random_node()
+        return message
