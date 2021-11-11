581517a11c772cbba71c7bf7addbcc6c3c5b7f62
NoodleSushi <ytmikesstuffs@gmail.com>
2021-11-11 16:37:14 +0800

    Hello Luigi

---
+++ main.py
@@ -0,0 +1,9 @@
+from typing import List, Dictionary
+
+class RandomMessageMaker():
+    def __init__(self) -> None:
+        pass
+    
+    def brain_generator(self, message_data: List[str]) -> Dictionary:
+        for message in message_data:
+            pass
